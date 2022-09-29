eb98b835c8bc702be79cdccb60a06a6cf74ec930
verayusifov <verayusif80@gmail.com>
2022-09-28 20:11:10 -0400

    first commit

---
+++ src/finalProject/finalClass.java
@@ -0,0 +1,11 @@
+package finalProject;
+
+public class finalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("This is the code for mercedes");	
+
+	}
+
+}
