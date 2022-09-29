c81767e7c999f7deb2442f18041134e53880dbaa
verayusifov <verayusif80@gmail.com>
2022-09-28 20:48:13 -0400

    second commit

---
+++ src/finalProject/finalProject1.java
@@ -0,0 +1,10 @@
+package finalProject;
+
+public class finalProject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is the code for Audi");
+	}
+
+}
